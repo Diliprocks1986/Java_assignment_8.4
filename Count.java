d35868d7d41a5b509e688b573212a7b0fcceb0b5
Dilip pashi <dilip.pashi143@gmail.com>
2017-01-09 13:36:43 +0530

    rydrj
    fgjg

---
+++ Count.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Count {
+
+
+    public static void main(String[] args) throws Exception {
+
+
+        java.io.File file = new java.io.File("E:\\file2.txt");
+
+ 
+
+        Scanner input = new Scanner(file);
+
+        String fileContent = "";
+
+
+        while (input.hasNext()) {
+
+            fileContent += input.next() + " ";
+
+        }
+
+     
+
+        input.close();
+
+        char[] charArr = fileContent.toCharArray();
+
+        int counter = 0;
+
+        for(char c : charArr)
+
+        {
+
+              if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y')
+
+                   counter++;
+
+        }
+
+        
+
+        
+        System.out.println("Number of Vowels: " + counter);
+
+    }
+}
